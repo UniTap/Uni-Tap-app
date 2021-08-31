eaa4dd5b31c5ee4ea556858eb520fbfe32afb86e
smil <smil.raj.thakur@gmail.com>
2021-09-01 01:51:15 +0530

    validation error

--- unitap/lib/widget/inputEmail.dart
+++ unitap/lib/widget/inputEmail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:unitap/pages/home.dart';
+import 'package:unitap/widget/button.dart';
 
 class InputEmail extends StatefulWidget {
   @override
@@ -13,17 +15,24 @@ class _InputEmailState extends State<InputEmail> {
       child: Container(
         height: 60,
         width: MediaQuery.of(context).size.width,
-        child: TextField(
-          style: TextStyle(
-            color: Colors.white,
-          ),
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            fillColor: Colors.lightBlueAccent,
-            labelText: 'Name',
-            labelStyle: TextStyle(
-              color: Colors.white70,
+        child: Form(
+          child: TextFormField(
+            style: TextStyle(
+              color: Colors.white,
             ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              fillColor: Colors.orange,
+              labelText: 'Name',
+              labelStyle: TextStyle(
+                color: Colors.white70,
+              ),
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "email cannot be empty";
+              }
+            },
           ),
         ),
       ),
