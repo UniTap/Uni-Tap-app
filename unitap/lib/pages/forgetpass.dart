9615fadf112ff0fb4ef78a252f496f4512539463
smil <smil.raj.thakur@gmail.com>
2021-09-14 15:35:37 +0530

    validation added

--- unitap/lib/pages/forgetpass.dart
+++ unitap/lib/pages/forgetpass.dart
@@ -7,6 +7,17 @@ class ForgetPasswordPage extends StatefulWidget {
 }
 
 class _ForgetPageState extends State<ForgetPasswordPage> {
+  bool ok = true;
+  final _formKey = GlobalKey<FormState>();
+  okpressed(BuildContext context) async {
+    if (_formKey.currentState!.validate() && ok == true) {
+      const snackBar =
+          SnackBar(content: Text('Your Email has recieved new Password!!'));
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,61 +30,64 @@ class _ForgetPageState extends State<ForgetPasswordPage> {
         ),
         child: ListView(
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                Row(children: <Widget>[
-                  //********** Header Part ***********//
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    //********** Header Part ***********//
 
-                  // ROTATED TEXT
-                  Padding(
-                    padding: const EdgeInsets.only(top: 60, left: 10),
-                    child: RotatedBox(
-                        quarterTurns: -1,
-                        child: Text(
-                          'New Password',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 38,
-                            fontWeight: FontWeight.w900,
-                          ),
-                        )),
-                  ),
+                    // ROTATED TEXT
+                    Padding(
+                      padding: const EdgeInsets.only(top: 60, left: 10),
+                      child: RotatedBox(
+                          quarterTurns: -1,
+                          child: Text(
+                            'New Password',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 38,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          )),
+                    ),
 
-                  // HEADER TEXT
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30.0, left: 10.0),
-                    child: Container(
-                      // color: Colors.orange[300],
-                      height: 200,
-                      width: 200,
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            height: 60,
-                          ),
-                          Center(
-                            child: Text(
-                              'Enter Email for Recovery',
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: Colors.white,
+                    // HEADER TEXT
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30.0, left: 10.0),
+                      child: Container(
+                        // color: Colors.orange[300],
+                        height: 200,
+                        width: 200,
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              height: 60,
+                            ),
+                            Center(
+                              child: Text(
+                                'Enter Email for Recovery',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ]),
+                  ]),
 
-                //********** Form Part ***********//
-                // EMAIL INPUT
-                Padding(
-                  padding: const EdgeInsets.only(top: 50, left: 50, right: 50),
-                  child: Container(
-                    height: 60,
-                    width: MediaQuery.of(context).size.width,
-                    child: Form(
+                  //********** Form Part ***********//
+                  // EMAIL INPUT
+
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 50, left: 50, right: 50),
+                    child: Container(
+                      height: 60,
+                      width: MediaQuery.of(context).size.width,
                       child: TextFormField(
                         style: TextStyle(
                           color: Colors.white,
@@ -88,114 +102,118 @@ class _ForgetPageState extends State<ForgetPasswordPage> {
                         ),
                         validator: (value) {
                           if (value!.isEmpty) {
-                            return "email cannot be empty";
+                            ok = false;
+                            const snackBar = SnackBar(
+                                content:
+                                    Text('Please enter registered Email ID!'));
+
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          } else {
+                            ok = true;
                           }
                         },
                       ),
                     ),
                   ),
-                ),
 
-                // SUBMIT
-                Container(
-                  padding: const EdgeInsets.only(top: 40, right: 50, left: 200),
-                  child: Container(
-                    alignment: Alignment.bottomRight,
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.deepOrange,
-                          blurRadius:
-                              10.0, // has the effect of softening the shadow
-                          spreadRadius:
-                              1.0, // has the effect of extending the shadow
-                          offset: Offset(
-                            5.0, // horizontal, move right 10
-                            5.0, // vertical, move down 10
+                  // SUBMIT
+                  Container(
+                    padding:
+                        const EdgeInsets.only(top: 40, right: 50, left: 200),
+                    child: Container(
+                      alignment: Alignment.bottomRight,
+                      height: 50,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.deepOrange,
+                            blurRadius:
+                                10.0, // has the effect of softening the shadow
+                            spreadRadius:
+                                1.0, // has the effect of extending the shadow
+                            offset: Offset(
+                              5.0, // horizontal, move right 10
+                              5.0, // vertical, move down 10
+                            ),
                           ),
-                        ),
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          TextButton(
-                            onPressed: () {
-                              const snackBar = SnackBar(
-                                  content: Text(
-                                      'Your Email has recieved new Password!'));
-
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(snackBar);
-                            },
-                            child: Center(
-                              child: Text(
-                                'OK',
-                                style: TextStyle(
-                                  color: Colors.orange,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w700,
+                        ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                okpressed(context);
+                              },
+                              child: Center(
+                                child: Text(
+                                  'OK',
+                                  style: TextStyle(
+                                    color: Colors.orange,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w700,
+                                  ),
                                 ),
                               ),
                             ),
+                            Icon(
+                              Icons.arrow_forward,
+                              color: Colors.orange,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  //********** Form Over ***********//
+
+                  // BACK TO LOGIN
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 30),
+                    child: Container(
+                      alignment: Alignment.topRight,
+                      height: 20,
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Remembered?',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white70,
+                            ),
                           ),
-                          Icon(
-                            Icons.arrow_forward,
-                            color: Colors.orange,
+                          SizedBox(
+                            width: 10,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => LoginPage()));
+                            },
+                            child: Text(
+                              'Sign in',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.right,
+                            ),
                           ),
                         ],
                       ),
                     ),
                   ),
-                ),
-
-                //********** Form Over ***********//
-
-                // BACK TO LOGIN
-                Padding(
-                  padding: const EdgeInsets.only(top: 30, left: 30),
-                  child: Container(
-                    alignment: Alignment.topRight,
-                    height: 20,
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          'Remembered?',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white70,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => LoginPage()));
-                          },
-                          child: Text(
-                            'Sign in',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.white,
-                            ),
-                            textAlign: TextAlign.right,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
