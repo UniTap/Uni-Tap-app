8b666d7e3ede31d5d73f513d5c120c193632d2d6
smil <smil.raj.thakur@gmail.com>
2021-09-17 01:50:02 +0530

    feed page designing

--- unitap/lib/pages/feeds.dart
+++ unitap/lib/pages/feeds.dart
@@ -15,6 +15,7 @@ class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white70,
       body: FutureBuilder(
           future: readjsondata(),
           builder: (context, data) {
@@ -25,8 +26,73 @@ class _FeedPageState extends State<FeedPage> {
               return ListView.builder(
                   itemCount: items == null ? 0 : items.length,
                   itemBuilder: (context, index) {
-                    return Card(
-                      color: Colors.black,
+                    return Container(
+                      decoration: BoxDecoration(
+                        //border: Border.all(),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 5.0,
+                            spreadRadius: -1.5,
+                          )
+                        ],
+                      ),
+
+                      height: 200,
+                      //alignment: Alignment.topLeft,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                height: 50,
+                                width: 50,
+                                margin: EdgeInsets.fromLTRB(15, 15, 10, 15),
+                                child: CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage: NetworkImage(
+                                        items[index].photo.toString())),
+                                decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(100)),
+                              ),
+                              Container(
+                                  child: Text(
+                                items[index].name.toString(),
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ))
+                            ],
+                          ),
+                          Container(
+                            height: 100,
+                            width: 500,
+                            margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                // BoxShadow(
+                                //   color: Colors.orange,
+                                //   blurRadius: 5.0,
+                                //   spreadRadius: -1.5,
+                                // )
+                              ],
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                items[index].tweet.toString(),
+                                style: TextStyle(fontSize: 20.0),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      margin: EdgeInsets.all(10),
                     );
                   });
             } else {
--- unitap/lib/pages/persondata.dart
+++ unitap/lib/pages/persondata.dart
@@ -3,13 +3,18 @@ class persondata {
   int? age;
   String? gender;
   int? enrno;
+  String? photo;
+  String? tweet;
 
-  persondata({this.name, this.age, this.gender, this.enrno});
+  persondata(
+      {this.name, this.age, this.gender, this.enrno, this.photo, this.tweet});
 
   persondata.fromJson(Map<String, dynamic> json) {
     name = json['name'];
     age = json['age '];
     gender = json['gender'];
     enrno = json['Enrollment number'];
+    photo = json['photo'];
+    tweet = json['tweet'];
   }
 }
